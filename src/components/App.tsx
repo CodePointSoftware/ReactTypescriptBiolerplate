a41bb553e3156a831b063c8507aa871a0f4f6124
Krzysztof <k.growinski@outlook.com>
2019-05-21 07:58:15 +0200

    adding simple component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component<{}, {}> {
-    render() {
-        return <h1>This is a Basic Typescript Component</h1>;
-    }
-}
+type Props = {
+    title?: string;
+    initialValue?: number;
+};
+
+const App: React.FunctionComponent<Props> = ({ title = 'Counter', initialValue = 0 }) => {
+    const [counter, setCounter] = useState(initialValue);
+
+    const increment = () => setCounter(counter + 1);
+    const decrement = () => setCounter(counter - 1);
+
+    return (
+        <>
+            <h1>
+                This is a Basic Typescript Component with {title}: {counter}
+            </h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </>
+    );
+};
 
 export default App;
