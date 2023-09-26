89fca4c509d1bb6d07dd95a5db3539700c5636a6
Krzysztof Growiński <k.growinski@outlook.com>
2023-09-26 13:12:21 +0200

    Migrate to vite

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,17 @@
 module.exports = {
-  semi: true,
-  trailingComma: "all",
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 4
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 2,
+    importOrder: [
+        '^react(.*)$',
+        '^[^@.+](.*)',
+        '^@(.*)',
+        '(.*)(/store/)(.*)',
+        '(.*)(/utils/)(.*)',
+        '^[.+/]',
+        '(.*)(sass|scss)$',
+    ],
+    importOrderSeparation: true,
 };
