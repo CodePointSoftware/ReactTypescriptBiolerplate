aeac8cf3a4f358c43b7ff3dcf546d4956c01433f
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-11-08 18:20:32 +0100

    Refactor PageList component with Redux Toolkit

--- src/pages/list/list.tsx
+++ src/pages/list/list.tsx
@@ -1,32 +1,34 @@
-import { FC, useEffect, useReducer } from 'react';
+import { FC, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { DetailItem } from '../detail/details';
 import { IHero } from '../../models/hero/hero';
 import './list.scss';
-import { GetData, SetData, SetError, initialState, reducer } from './listReducer';
+import { getDataStart, getDataSuccess, getDataError } from './listSlice';
+import { useAppDispatch } from '../../../src/store/root';
 import femalePhoto from '../detail/images/female.png'
 import malePhoto from '../detail/images/male.png';
 import emptyPhoto from '../detail/images/mark.png';
 
-
 const PageList: FC = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const data = useSelector((state: any) => state.list.data);
+  const isLoading = useSelector((state: any) => state.list.isLoading)
+  const isError = useSelector((state: any) => state.list.error);
 
-  const getHeroes = async () => {
-    try {
-      dispatch(GetData());
-      const response = await fetch('https://swapi.dev/api/people');
-      const data = await response.json();
-      dispatch(SetData(data.results));
-    } catch (err) {
-      dispatch(SetError());
-    }
-  };
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    getHeroes();
-  }, []);
+    dispatch(getDataStart());
+    fetch('https://swapi.dev/api/people')
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(getDataSuccess(data.results));
+      })
+      .catch((_err) => {
+        dispatch(getDataError('Error fetching data'));
+      });
+  }, [dispatch]);
 
-  const renderHeroes = state.data?.map((hero: IHero) => (
+  const renderHeroes = data?.map((hero: IHero) => (
     <div className="list-item" key={hero.name}>
       {hero.gender !== 'n/a' && (
       <img
@@ -45,7 +47,13 @@ const PageList: FC = () => {
       
     <DetailItem
         hero={hero}
-        onclick={() => alert(`Hero detils: ${hero.name}`)} key={''} url={''} femalePhoto={''} malePhoto={''} emptyPhoto={''}/>
+        onclick={() => alert(`Hero detils: ${hero.name}`)} 
+        key={''} 
+        url={''} 
+        femalePhoto={''} 
+        malePhoto={''} 
+        emptyPhoto={''}
+        />
     </div>
   ));
 
@@ -53,8 +61,11 @@ const PageList: FC = () => {
     <div className="page-list__wrapper">
       <div className="page-list">
         <h1 className="page-list__header">Star Wars Heroes</h1>
-        {state.isLoading && <p className="page-list__loading">Loading...</p>}
-        {!state.isLoading && <div className="page-list__list">{renderHeroes}</div>}
+        {isLoading && <p className="page-list__loading">Loading...</p>}
+        {!isLoading && !isError && (
+          <div className="page-list__list">{renderHeroes}</div>
+        )}
+        {isError && <p className="page-list__error">Error fetching data.</p>}
       </div>
     </div>
   );
